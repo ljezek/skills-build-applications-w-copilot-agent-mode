026df5dea1f2689ef50ac2ad3177cb06e9b562e3
ljezek <102726803+ljezek@users.noreply.github.com>
2025-04-09 12:40:08 +0000

    Launch settings for the app in my codespace.

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,19 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
 from .models import User, Team, Activity, Leaderboard, Workout
+from django.http import JsonResponse
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    base_url = 'http://127.0.0.1:8000/'
-    return Response({
-        'users': base_url + 'api/users/',
-        'teams': base_url + 'api/teams/',
-        'activities': base_url + 'api/activities/',
-        'leaderboard': base_url + 'api/leaderboard/',
-        'workouts': base_url + 'api/workouts/'
-    })
+def api_root(request):
+    return JsonResponse({"message": "Welcome to the Octofit API!", "url": "https://studious-parakeet-jpwxjgjx46jf6-8000.app.github.dev"})
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
